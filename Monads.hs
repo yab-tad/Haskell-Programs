1a5883ff8b21742c08540c05db840c1ad9905d71
MightyJasper <tadesse.yeabsira18@gmail.com>
2022-08-31 11:43:53 +0300

    a little bit more on monads

--- Monads.hs
+++ Monads.hs
@@ -1,4 +1,6 @@
---
+
+import Control.Monad
+
 {-
 
 {-# LANGUAGE KindSignatures #-}
@@ -50,4 +52,68 @@ monadd mx my = do
         Just 3
         ghci> monadd (Just 1) Nothing
         Nothing
+-}
+
+
+data Writer' a = Writer' a [String]
+    deriving (Show)
+
+instance Functor Writer' where
+    fmap = liftM
+
+instance Applicative Writer' where
+    pure = return
+    (<*>) = ap
+
+instance Monad Writer' where
+    return a = Writer' a []
+    (>>=) = bindWriter
+
+number :: Int -> Writer' Int
+number n = Writer' n ["number: " ++ show n]
+
+tell :: [String] -> Writer' ()
+tell = Writer' ()
+
+bindWriter :: Writer' a -> (a -> Writer' b) -> Writer' b
+bindWriter (Writer' a xs) f =
+    let Writer' b ys = f a
+    in
+        Writer' b $ xs ++ ys
+
+threeInts :: Monad m => m Int -> m Int -> m Int -> m Int
+threeInts mx my mz = 
+    mx >>= \k ->
+        my >>= \l ->
+            mz >>= \m -> 
+                let s = k + l + m in return s
+
+threeInts' :: Monad m => m Int -> m Int -> m Int -> m Int
+threeInts' mx my mz = do
+    k <- mx
+    l <- my
+    m <- mz
+    let s = k + l + m
+    return s
+
+foo :: Writer' Int -> Writer' Int -> Writer' Int -> Writer' Int
+foo x y z =
+    x `bindWriter` \k ->
+        y  `bindWriter` \l ->
+            z `bindWriter` \m -> 
+            let s = k + l + m
+            in tell ["Sum : " ++ show s] `bindWriter` \_ ->
+                Writer' s []
+
+foo' :: Writer' Int -> Writer' Int -> Writer' Int -> Writer' Int
+foo' x y z = do
+    s <- threeInts x y z
+    tell ["sum : " ++ show s]
+    return s
+
+{-
+        *Main> foo (number 1) (number 2) (number 3)
+                Writer' 6 ["number: 1","number: 2","number: 3","Sum : 6"]
+        *Main> foo' (number 1) (number 2) (number 3)
+                Writer' 6 ["number: 1","number: 2","number: 3","sum : 6"]
 -}
